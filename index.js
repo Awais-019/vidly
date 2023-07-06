9f3ad2de2d8e658cbfc697fd9d0d0e008028f1a6
Muhammad Awais <awaistopper20@gmail.com>
2023-07-06 13:06:56 +0500

    Added endpoint for genres

---
+++ index.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const app = express();
+
+app.use(express.json());
+
+const genres = [
+  { id: 1, name: "Action" },
+  { id: 2, name: "Horror" },
+  { id: 3, name: "Romance" },
+];
+
+app.get("/", (req, res) => {
+  res.send("Welcome to the Vidly!");
+});
+
+app.get("/api/genres", (req, res) => {
+  res.send(genres);
+});
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => console.log(`Listening on port ${port}...`));
