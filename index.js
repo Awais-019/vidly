516387de2c5d9051433e0500a8862f88c75ec0bf
Muhammad Awais <awaistopper20@gmail.com>
2023-07-10 14:02:09 +0500

    Implemented customers api

--- index.js
+++ index.js
@@ -13,10 +13,12 @@ mongoose
 
 const genres = require("./routes/genres");
 const home = require("./routes/home");
+const customers = require("./routes/customers");
 
 app.use(express.json());
 app.use("/", home);
 app.use("/api/genres", genres);
+app.use("/api/customers", customers);
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => console.log(`Listening on port ${port}...`));
