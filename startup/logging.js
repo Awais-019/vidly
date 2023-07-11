517359c5fbb66b96a71a6e93b43fed7b834fa788
Muhammad Awais <awaistopper20@gmail.com>
2023-07-11 12:25:54 +0500

    Logging errors to mongodb

--- startup/logging.js
+++ startup/logging.js
@@ -1,7 +1,11 @@
 const { createLogger, format, transports } = require("winston");
 const { combine, timestamp } = format;
+require("winston-mongodb");
 
 module.exports = createLogger({
   format: combine(timestamp(), format.json()),
-  transports: [new transports.File({ filename: "combined.log" })],
+  transports: [
+    new transports.File({ filename: "combined.log" }),
+    new transports.MongoDB({ db: "mongodb://localhost/vidly", level: "error" }),
+  ],
 });
