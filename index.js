d6cba580ddbf0bcae06d197a1527e917fc27728c
Muhammad Awais <awaistopper20@gmail.com>
2023-07-11 12:38:27 +0500

    Handling unhandled rejected promises

--- index.js
+++ index.js
@@ -46,6 +46,11 @@ process.on("uncaughtException", (ex) => {
   process.exit(1);
 });
 
+process.on("unhandledRejection", (ex) => {
+  console.log("WE GOT AN UNHANDLED REJECTION");
+  process.exit(1);
+});
+
 const port = process.env.PORT || 3000;
 app.listen(port, () => console.log(`Listening on port ${port}...`));
 
