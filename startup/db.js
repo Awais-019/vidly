b7bf393fcb1cd2839709b25b5e3782870f2bcad6
Muhammad Awais <awaistopper20@gmail.com>
2023-07-11 20:35:43 +0500

    Preparing test database

--- startup/db.js
+++ startup/db.js
@@ -1,8 +1,11 @@
 const mongoose = require("mongoose");
+const config = require("config");
+
 const infoLogger = require("./logging");
 
 module.exports = function () {
-  mongoose.connect("mongodb://localhost/vidly").then(() => {
-    infoLogger.info("Connected to MongoDB...");
+  const db = config.get("db");
+  mongoose.connect(db).then(() => {
+    infoLogger.info(`Connected to ${db}...`);
   });
 };
