1f4f4cc8d051045854548e6f581a3daa590ed517
Muhammad Awais <awaistopper20@gmail.com>
2023-07-11 12:47:31 +0500

    Extracted routes logic out of index.js

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ Joi.objectId = require("joi-objectid")(Joi);
 const mongoose = require("mongoose");
 require("express-async-errors");
 require("./startup/logging");
+require("./startup/routes")(app);
 
 mongoose
   .connect("mongodb://localhost/vidly")
@@ -16,30 +17,10 @@ mongoose
     console.error("Could not connect to MongoDB...", err.message)
   );
 
-const genres = require("./routes/genres");
-const home = require("./routes/home");
-const customers = require("./routes/customers");
-const movies = require("./routes/movies");
-const rentals = require("./routes/rentals");
-const users = require("./routes/users");
-const auth = require("./routes/auth");
-const error = require("./middlewares/error");
-
-app.use(error);
-
 if (!config.get("jwtPrivateKey")) {
   console.error("FATAL ERROR: jwtPrivateKey is not defined");
   process.exit(1);
 }
-
-app.use(express.json());
-app.use("/", home);
-app.use("/api/genres", genres);
-app.use("/api/customers", customers);
-app.use("/api/movies", movies);
-app.use("/api/rentals", rentals);
-app.use("/api/users", users);
-app.use("/api/auth", auth);
 
 process.on("uncaughtException", (ex) => {
   console.log("WE GOT AN UNCAUGHT EXCEPTION");
