dccaec9df59b2ccccabfa099d9b914f9eeac5153
Muhammad Awais <muhammad.awais.20024@gmail.com>
2023-07-13 17:46:15 +0500

    Refactored validation

--- routes/returns.js
+++ routes/returns.js
@@ -1,16 +1,14 @@
 const express = require("express");
 const moment = require("moment");
+const Joi = require("../startup/validation");
+const validate = require("../middlewares/validate");
 const auth = require("../middlewares/auth");
 const router = express.Router();
 
 const { Rental } = require("../models/rentals");
 const { Movie } = require("../models/movies");
 
-router.post("/", auth, async (req, res) => {
-  if (!req.body.customerId)
-    return res.status(400).send("customerId not provided");
-  if (!req.body.movieId) return res.status(400).send("movieId not provided");
-
+router.post("/", [auth, validate(validateReturn)], async (req, res) => {
   const rental = await Rental.findOne({
     "customer._id": req.body.customerId,
     "movie._id": req.body.movieId,
@@ -36,5 +34,13 @@ router.post("/", auth, async (req, res) => {
 
   return res.status(200).send(rental);
 });
+
+function validateReturn(req) {
+  const schema = Joi.object({
+    customerId: Joi.objectId().required(),
+    movieId: Joi.objectId().required(),
+  });
+  return schema.validate(req.body);
+}
 
 module.exports = router;
