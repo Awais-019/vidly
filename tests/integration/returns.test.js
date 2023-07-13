2bda5e81efd29a7d598b496dfcb615aed34ab89d
Muhammad Awais <muhammad.awais.20024@gmail.com>
2023-07-13 16:15:51 +0500

    400 test cases

--- tests/integration/returns.test.js
+++ tests/integration/returns.test.js
@@ -1,6 +1,7 @@
 const mongoose = require("mongoose");
 const request = require("supertest");
 const { Rental } = require("../../models/rentals");
+const { User } = require("../../models/users");
 
 describe("/api/returns", () => {
   let server;
@@ -36,5 +37,23 @@ describe("/api/returns", () => {
       .post("/api/returns")
       .send({ customerId, movieId });
     expect(res.status).toBe(401);
+  });
+
+  it("should return 400 if customerId is not provided", async () => {
+    const token = new User().generateAuthToken();
+    const res = await request(server)
+      .post("/api/returns")
+      .set("x-auth-token", token)
+      .send({ movieId });
+    expect(res.status).toBe(400);
+  });
+
+  it("should return 400 if movieId is not provided", async () => {
+    const token = new User().generateAuthToken();
+    const res = await request(server)
+      .post("/api/returns")
+      .set("x-auth-token", token)
+      .send({ customerId });
+    expect(res.status).toBe(400);
   });
 });
